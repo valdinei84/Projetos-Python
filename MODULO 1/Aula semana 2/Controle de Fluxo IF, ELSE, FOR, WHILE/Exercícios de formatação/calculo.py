9c37a8d8329c29d6f88c98c042b918847cb48c72
Isadora Garcia Ferrão <isadora-gf@hotmail.com>
2023-03-01 00:23:00 +0100

    Update calculo.py

--- MODULO 1/Aula semana 2/Controle de Fluxo IF, ELSE, FOR, WHILE/Exercícios de formatação/calculo.py
+++ MODULO 1/Aula semana 2/Controle de Fluxo IF, ELSE, FOR, WHILE/Exercícios de formatação/calculo.py
@@ -4,7 +4,7 @@ o resultado da soma e da multiplicação, de um número pelo outro.
 '''
 from numbers import Real
 
-
+##
 numero1 = float(input ("Informe um número "))
 numero2 = float(input ("Informe outro número "))
 resultado_soma = numero1 + numero2
